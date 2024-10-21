dd66a16c680397a72270f2e1930d5604c29da7a5
Pietro <pietro.bambagioni@stud.unifi.it>
2024-10-21 19:03:35 +0200

    creato e inixiato il progetto

---
+++ ABR.py
@@ -0,0 +1,6 @@
+class AlberoBinarioRicerca:
+    def __init__(self, Contenuto, Sinistra=None, Destra=None):
+        self.Contenuto = Contenuto
+        self.Sinistra = Sinistra
+        self.Destra = Destra
+        
