1cc0226164647844cf9f40d14bf110d60ab167dc
Pietro <pietro.bambagioni@stud.unifi.it>
2024-10-22 19:01:38 +0200

    fix delete, created ger_kesimo

--- ABR.py
+++ ABR.py
@@ -4,6 +4,11 @@ class Abr:
         # imposto il valore del nodo o crea un nodo vuoto
         self.key = key
         self.parent = parent
+        # imposta la radice
+        if not parent: # se non ha un padre è la radice
+            self.root = self
+        else: # altrimenti la radice è la radice del padre
+            self.root = parent.root
         # se non è vuoto imposto i figli come nodi vuoti
         if self.key:
             print("l'albero è stato creato con successo e {} è la sua radice".format(self.key))
@@ -17,6 +22,12 @@ class Abr:
     def isempty(self):
         return self.key is None
 
+    def isroot(self):
+        if self.root == self:
+            return True
+        else:
+            return False
+
     def notempty(self):
         return self.key is not None
 
@@ -25,7 +36,6 @@ class Abr:
             print("Il nodo padre di {} è {}".format(self.key, self.parent.key))
         else:
             print("Questo nodo non ha un padre (è la radice)")
-
 
     # inserisci valore
     def insert(self, value):
@@ -79,11 +89,6 @@ class Abr:
             self = self.left
         print("il valore minimo è: {}".format(self.key))
 
-    def min(self):
-        # controllo se il nodo a sinistra è vuoto
-        while not self.left.isempty():
-            self = self.left
-        return self
 
     def max(self):
         # controllo se il nodo a destra è vuoto
@@ -91,50 +96,85 @@ class Abr:
             self = self.right
         print("il valore massimo è: {}".format(self.key))
 
-    def successor(value):
-        if value.right.isempty():
+    def min(self):
+        current = self
+        # controllo se il nodo a sinistra è vuoto
+        while not current.left.isempty():
+            current = current.left
+        return current
+    def minroot(root):
+        x = root
+        # controllo se il nodo a sinistra è vuoto
+        while not x.left.isempty():
+            x = x.left
+        return x
+
+    def successor(self):
+        if self.right.isempty():
             return None
-        next = value.right
-        while next is not None and next.left is not None:
+        next = self.right
+        while not next.left.isempty():
             next = next.left
         return next
 
     def trasplant(self, u, v):
-        if u.parent.isempty():
-            self = v
+        if u.parent is None:
+            self.root = v
         elif u == u.parent.left:
             u.parent.left = v
         else:
             u.parent.right = v
-        if  v.isnotempty():
+        if  v.notempty():
             v.parent = u.parent
 
     def delete(self, value):
-        root = self
+        current = self
         #trovo il nodo da eliminare
-        while root.key != value:
-            if root.isempty():  # il nodo non è presente
+        while current.key != value:
+            if current.isempty():  # il nodo non è presente
                 print("il valore: {} non è presente nell'albero".format(value))
                 return
-            if value < root.key: # il valore è minore del nodo attuale cerco a sinistra
-                root = root.left
+            if value < current.key: # il valore è minore del nodo attuale cerco a sinistra
+                current = current.left
             else:
-                root = root.right # il valore è maggiore del nodo attuale cerco a destra
-        # caso 1: il nodo da eliminare non ha figli o ha solo il figlio destro
-        z = root
+                current = current.right # il valore è maggiore del nodo attuale cerco a destra
+        z = current
+        # Caso 1: il nodo da eliminare non ha figli o ha solo il figlio destro
         if z.left.isempty():
-            z.trasplant(z, z.right)
-        # caso 2: il nodo da eliminare ha solo il figlio sinistro
+            self.trasplant(z, z.right)
+        # Caso 2: il nodo da eliminare ha solo il figlio sinistro
         elif z.right.isempty():
-             z.trasplant(z, z.left)
-        # caso 3: il nodo da eliminare ha entrambi i figli
+             self.trasplant(z, z.left)
+        # Caso 3: il nodo da eliminare ha entrambi i figli
         else:
-            y = z.right.min()
-            if y.parent != value:
-                z.trasplant(y, y.right)
+            y = z.right.minroot()
+            if y.parent != z:
+                self.trasplant(y,y.right)
                 y.right = z.right
                 y.right.parent = y
-            z.trasplant(z, y)
+            self.trasplant(z,y)
             y.left = z.left
             y.left.parent = y
         print("il valore: {} è stato eliminato con successo".format(value))
+
+    def get_kesimo(root, k):
+        stack = []
+        current = root
+        count = 0
+        while True:
+            # scendo a sinistra fino a trovare un nodo vuoto e metto i nodi visitati nello stack
+            if current.left is not None:
+                stack.append(current)
+                current = current.left
+            # prendo il nodo in cima allo stack e lo visito
+            elif stack:
+                current = stack.pop()
+                count += 1
+                if count == k:
+                    print("il {}° nodo è: {}".format(k, current.key))
+                    return current.key
+                current = current.right
+            else:
+                break
+
+        return None
